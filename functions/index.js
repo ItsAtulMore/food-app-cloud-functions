c262ddd1c47cc64e6336cf4dce47649c27de0582
itsme-atul <hariom23shah@gmail.com>
2022-02-05 15:25:19 +0530

    atul

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,13 @@
 const functions = require("firebase-functions");
-
+var cors = require("cors");
 const express = require("express");
 const app = express();
+app.use(cors());
+
 const port = 3000;
 
 app.get("/fetchApiPath/:clientId", (req, res) => {
-    console.log(req.params);
+  console.log(req.params);
   let pClientId = req.params.clientId;
   let data = [];
 
@@ -34,6 +36,14 @@ app.get("/fetchApiPath/:clientId", (req, res) => {
       subscriptionType: "GOLD",
       subcriptionEndDate: "2023-01-27",
     },
+    {
+      clientId: "HSRETAIL",
+      serverName: "Office Server",
+      serverPath: "http://103.140.27.5:4002/",
+      secretKey: "ABC1234",
+      subscriptionType: "GOLD",
+      subcriptionEndDate: "2023-01-27",
+    },
   ];
 
   data = clientsData.filter((xx) => xx.clientId === pClientId);
@@ -44,4 +54,3 @@ app.get("/fetchApiPath/:clientId", (req, res) => {
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.app = functions.https.onRequest(app);
-
